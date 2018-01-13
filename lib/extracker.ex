e9ef2bd04f8c32e65c211031da4c2031edc12268
Bobby Richter <robert.c.richter@gmail.com>
2018-01-12 18:02:29 -0600

    Turn the tracker module into a genserver
    It didn't take long for this to outgrow an agent

--- lib/extracker.ex
+++ lib/extracker.ex
@@ -1,29 +1,28 @@
 defmodule Extracker do
-  use Agent
+  use GenServer
 
   def start_link(_) do
-    start_link()
-  end
-
-  def start_link do
-    Agent.start_link(fn -> MapSet.new end, name: __MODULE__)
+    GenServer.start_link(__MODULE__, %{peers: MapSet.new}, name: __MODULE__)
   end
 
   def request(%{info_hash: _, peer_id: _, port: _, uploaded: _, downloaded: _, left: _, ip: _} = req) do
-    peer = Map.take(req, [:peer_id, :ip, :port])
-    peers = Agent.get_and_update(__MODULE__, &put_and_return(&1, peer))
-    %{
-      interval: 9_000,
-      peers: MapSet.to_list(peers)
-    }
+    GenServer.call(__MODULE__, {:announce, req})
   end
 
   def request(_req) do
     %{ failure_reason: "invalid request" }
   end
 
-  defp put_and_return(set, elem) do
-    updated = MapSet.put(set, elem)
-    {updated, updated}
+  def handle_call({:announce, req}, _from, state) do
+    peer = Map.take(req, [:peer_id, :ip, :port])
+    peers = MapSet.put(state.peers, peer)
+    {
+      :reply,
+      %{
+        interval: 9_000,
+        peers: MapSet.to_list(peers)
+      },
+      %{state | peers: peers}
+    }
   end
 end

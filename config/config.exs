b8e76393eba5e41e72bc69a0255855b753f045f6
Bobby Richter <robert.c.richter@gmail.com>
2018-01-16 10:28:05 -0600

    Clean up duplication in config file
    I didn't realize until now that you could use keyword lists in config files.

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,6 @@
 use Mix.Config
 
-config :extracker, :interval, 9_000 # 2 hours, 30 minutes
-config :extracker, :path, "/"
-config :extracker, :port, 6969
+config :extracker,
+  interval: 9_000, # 2 hours, 30 minutes
+  path: "/",
+  port: 6969

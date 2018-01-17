3d3854b512be006d7e49018d4ef98b85f634952d
Rosa Richter <rosa.richter@protonmail.com>
2018-01-17 11:45:08 -0600

    Refactor HTTP handler to prepare for library call

--- lib/extracker/http/handler.ex
+++ lib/extracker/http/handler.ex
@@ -3,18 +3,35 @@ defmodule Extracker.HTTP.Handler do
   Handle THP connections.
   """
   @behaviour :cowboy_handler
+  require ExBencode
 
   ## Callbacks
-  
-  def init(req, state) do
 
-    res = :cowboy_req.reply(
+  def init(req, state) do
+    with {:ok, query_result} <- query(req),
+         {:ok, body} <- ExBencode.encode(query_result),
+         res <- response(body, req)
+    do
+      {:ok, res, state}
+    else
+      _ -> {:ok, failure_body(req), state}
+    end
+  end
+
+  defp query(_req) do
+    :error
+  end
+
+  defp failure_body(req) do
+    response "d14:failure reason19:service unavailablee", req
+  end
+
+  defp response(body, req) do
+    :cowboy_req.reply(
       200,
       %{"content-type" => "text/plain"},
-      "d14:failure reason19:service unavailablee",
+      body,
       req
     )
-
-    {:ok, res, state}
   end
 end

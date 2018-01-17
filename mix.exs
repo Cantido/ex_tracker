a544fef83d833813e9e6a0e5b98a18b2114c2fb2
Rosa Richter <rosa.richter@protonmail.com>
2018-01-17 11:13:58 -0600

    Use the correct version of Elixir
    I had a bad Elixir version installed by my package manager.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Extracker.Mixfile do
     [
       app: :extracker,
       version: "0.0.1",
-      elixir: "~> 1.1-dev",
+      elixir: "~> 1.5",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),

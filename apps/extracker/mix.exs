7e28e42edf07c0d4772261d5af54f3ea3b3bbe35
Rosa Richter <rosa.richter@protonmail.com>
2018-01-31 13:16:50 -0600

    Update extracker to elixir 1.6

--- apps/extracker/mix.exs
+++ apps/extracker/mix.exs
@@ -5,7 +5,7 @@ defmodule Extracker.MixProject do
     [
       app: :extracker,
       version: "0.0.1",
-      elixir: "~> 1.5",
+      elixir: "~> 1.6",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",

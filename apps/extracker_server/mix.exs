7b150d2004b4b14bed460766a5ec6499a786675c
Rosa Richter <rosa.richter@protonmail.com>
2018-01-31 13:19:01 -0600

    Rename server

--- apps/extracker_server/mix.exs
+++ apps/extracker_server/mix.exs
@@ -3,7 +3,7 @@ defmodule ExtrackerPlug.MixProject do
 
   def project do
     [
-      app: :extracker_plug,
+      app: :extracker_server,
       version: "0.1.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",

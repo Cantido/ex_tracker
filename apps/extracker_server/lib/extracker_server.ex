5b75678c7f72421d938341e1827850fd1c4d6479
Rosa Richter <rosa.richter@protonmail.com>
2018-01-31 13:38:00 -0600

    Add a scrape URL

--- apps/extracker_server/lib/extracker_server.ex
+++ apps/extracker_server/lib/extracker_server.ex
@@ -22,6 +22,16 @@ defmodule ExtrackerServer do
     |> send_resp()
   end
 
+  get "/scrape" do
+    hash = conn.query_params[:info_hash]
+    result = case Extracker.scrape(hash) do
+      %{"failure_reason" => _} = failure -> failure
+      success -> %{files: %{hash => success}}
+    end
+    {:ok, body} = ExBencode.encode(result)
+    send_resp(conn, 200, body)
+  end
+
   match _ do
     send_resp(conn, 404, "oops")
   end
--- apps/extracker_server/test/extracker_server_test.exs
+++ apps/extracker_server/test/extracker_server_test.exs
@@ -84,4 +84,50 @@ defmodule ExtrackerServerTest do
   #
   #   assert body_data["peers"] == expected_peer
   # end
+
+  test "returns an empty scrape if there have been no announcements" do
+    query = URI.encode_query(%{"info_hash" => "Torrent with no DLs-"})
+    conn = conn(:get, "/scrape?" <> query)
+    conn = ExtrackerServer.call(conn, @opts)
+
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert {:ok, body} = ExBencode.decode(conn.resp_body)
+
+    assert Map.keys(body) == ["files"]
+    assert body["files"] == %{"Torrent with no DLs-" => %{"complete" => 0, "downloaded" => 0, "incomplete" => 0}}
+  end
+
+  test "returns a scrape with a torrent" do
+    announce_request = %{
+      info_hash: "Scraped Info Hash---",
+      peer_id: "Peer ID One---------",
+      port: 56420,
+      ip: "69.69.69.69",
+      uploaded: 0,
+      downloaded: 0,
+      left: 5000000
+    }
+    announce_conn =
+      conn(:get, "/announce?" <> URI.encode_query(announce_request))
+      |> ExtrackerServer.call(@opts)
+    assert announce_conn.state == :sent
+    assert announce_conn.status == 200
+
+    scrape_request = %{
+      info_hash: "Scraped Info Hash---"
+    }
+    scrape_conn =
+      conn(:get, "/scrape?" <> URI.encode_query(scrape_request))
+      |> ExtrackerServer.call(@opts)
+    assert scrape_conn.state == :sent
+    assert scrape_conn.status == 200
+
+    assert {:ok, body} = ExBencode.decode(scrape_conn.resp_body)
+
+    assert Map.keys(body) == ["files"]
+    files = body["files"]
+    assert Map.keys(files) == ["Scraped Info Hash---"]
+    assert files == %{"Scraped Info Hash---" => %{"complete" => 0, "downloaded" => 0, "incomplete" => 1}}
+  end
 end

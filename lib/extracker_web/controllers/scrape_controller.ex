2269a00c629192f3766c97060d0d4805adc3f1d6
Rosa Richter <cosmic.lady.rosa@gmail.com>
2019-09-10 18:27:53 -0500

    Add scrape back in

--- lib/extracker_web/controllers/scrape_controller.ex
+++ lib/extracker_web/controllers/scrape_controller.ex
@@ -2,6 +2,20 @@ defmodule ExtrackerWeb.ScrapeController do
   use ExtrackerWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    files = hashes(conn)
+    |> Stream.map(fn x -> {x, Extracker.scrape(x)} end)
+    |> Stream.reject(fn {k, _} -> k == "failure_reason" end)
+    |> Map.new
+
+    {:ok, body} = ExBencode.encode(%{files: files})
+
+    text(conn, body)
+  end
+
+  defp hashes(conn) do
+    conn.query_string
+    |> URI.query_decoder
+    |> Stream.filter(fn({k, _}) -> k == "info_hash" end)
+    |> Stream.map(fn({_, v}) -> v end)
   end
 end

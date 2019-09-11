a709167479901ba738a9ad23e0575747f3672640
Rosa Richter <cosmic.lady.rosa@gmail.com>
2019-09-11 09:31:26 -0500

    Fix a test

--- test/extracker_web/controllers/page_controller_test.exs
+++ test/extracker_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule ExtrackerWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "This is but a humble BitTorrent tracker. There is nothing to see here."
   end
 end
